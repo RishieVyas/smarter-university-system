db4eff094fe3a6b4591e8e6b8bd3254e7073df16
niveditaisrani <niveditaisrani@gmail.com>
2023-11-25 16:32:30 -0500

    added  test case of Quizzez controller

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,4 +1,5 @@
 import unittest
+import datetime
 
 from app.controllers.quizzes_controller import QuizzesController
 
@@ -8,13 +9,30 @@ class QuizzesTest(unittest.TestCase):
         # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
         
-    def test_expose_failure_01(self):
-        """
-        Implement this function and two more that
-        execute the code and make it fail.
-        """
-        self.assertTrue(True, 'Example assertion.')
         
+    def test_02_add_quiz_invalidInputFormat_assertLengthOfQuizzesAdded(self):
+        """
+       Tests the addition of a quiz with invalid input format.
+
+        1. Verifies the scenario causing Issue 1 in Line 24 of quizzes_controller.py.
+        2. Highlights the scenario causing Issue 2 in Line 16 of quizzes_controller.py.
+
+        Steps:
+        - Clears existing data.
+        - Adds a new quiz with invalid input format.
+        - Retrieves the list of quizzes.
+        - Checks the number of quizzes in the list and the generated quiz ID.
+
+        Asserts:
+        - Ensures that after adding a quiz with invalid input, the number of quizzes remains incorrect.
+        - Validates that the generated quiz ID doesn't match the expected value.
+        """
+        self.ctrl.clear_data()
+        quiz_id = self.ctrl.add_quiz("Quiz Title", "quiz 1", datetime.datetime(2023, 1, 2), datetime.datetime(2023, 1, 3))
+        # Check that we have one quiz in the list
+        quizzes = self.ctrl.get_quizzes()
+        self.assertEqual(len(quizzes), 2, "Error: There is only one discussion.")
+        self.assertEqual(quiz_id, "some quiz id", "Error: The quiz id generated is different")
 
 if __name__ == '__main__':
     unittest.main()
