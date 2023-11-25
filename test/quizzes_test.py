9db805ec87ed0a0702fb7a0047c6d22744253665
Rishi Vyas <rishievyas@gmail.com>
2023-11-25 16:30:10 -0500

    First Unit Test for add_quiz function

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,5 +1,5 @@
 import unittest
-
+import datetime
 from app.controllers.quizzes_controller import QuizzesController
 
 class QuizzesTest(unittest.TestCase):
@@ -10,11 +10,11 @@ class QuizzesTest(unittest.TestCase):
         
     def test_expose_failure_01(self):
         """
-        Implement this function and two more that
-        execute the code and make it fail.
+        The test case is failing at line 63 
+        in add_quiz of quizzes_controller.py file 
         """
-        self.assertTrue(True, 'Example assertion.')
-        
+        quiz_id = self.ctrl.add_quiz(None, "Text 1", datetime.datetime(2023, 11, 11), datetime.datetime(2023, 11, 13))
+        self.assertIsNone(quiz_id, 'Quiz gets added')
 
 if __name__ == '__main__':
     unittest.main()
