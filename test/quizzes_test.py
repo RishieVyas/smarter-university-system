45466f898b4cdc5fab1f8f5f0f1b4e8d592fa06a
Rishi Vyas <rishievyas@gmail.com>
2023-11-26 15:53:46 -0500

    Final changes for the unit test cases

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -11,44 +11,46 @@ class QuizzesTest(unittest.TestCase):
 
     def test_expose_failure_01(self):
         """
+        The add_quiz function expects string values for the 'title' argument, and passing None
+        results in a crash due to an unsupported data type. This test  identifies that the function is not handling
+        such a scenario
+        
         The test case is failing at line 63 
-        in add_quiz of quizzes_controller.py file 
+        in add_quiz function of quizzes_controller.py file 
         """
+        self.ctrl.clear_data()
         quiz_id = self.ctrl.add_quiz(None, "Text 1", datetime.datetime(2023, 11, 11), datetime.datetime(2023, 11, 13))
         self.assertIsNone(quiz_id, 'Quiz gets added')    
-        
-    def test_02_add_quiz_invalidInputFormat_assertLengthOfQuizzesAdded(self):
+
+    def test_expose_failure_02(self):
         """
-       Tests the addition of a quiz with invalid input format.
+        The code encounters a crash at line 94 at add_answer in quiz_controller.py file when attempting to add an answer.
+        The issue stems from passing a date as text in the add_answer function, and the program fails to handle
+        the exception appropriately. The current implementation lacks proper error handling, leading to a crash.
 
-        1. Verifies the scenario causing Issue 1 in Line 24 of quizzes_controller.py.
-        2. Highlights the scenario causing Issue 2 in Line 16 of quizzes_controller.py.
-
-        Steps:
-        - Clears existing data.
-        - Adds a new quiz with invalid input format.
-        - Retrieves the list of quizzes.
-        - Checks the number of quizzes in the list and the generated quiz ID.
-
-        Asserts:
-        - Ensures that after adding a quiz with invalid input, the number of quizzes remains incorrect.
-        - Validates that the generated quiz ID doesn't match the expected value.
         """
         self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz("Quiz Title", "quiz 1", datetime.datetime(2023, 1, 2), datetime.datetime(2023, 1, 3))
-        # Check that we have one quiz in the list
-        quizzes = self.ctrl.get_quizzes()
-        self.assertEqual(len(quizzes), 2, "Error: There is only one discussion.")
-        self.assertEqual(quiz_id, "some quiz id", "Error: The quiz id generated is different")
+        # Add a quiz
+        quiz_id = self.ctrl.add_quiz("Quiz Title", "quiz 2", datetime.datetime(2023, 1, 2), datetime.datetime(2023, 1, 3))
+        # Add a question
+        question_id = self.ctrl.add_question(quiz_id, "quiz 2", "question text")
+        # Adding an answer
+        anwer_id = self.ctrl.add_answer(question_id, datetime.datetime.now(), True)
+        self.assertIsNotNone(anwer_id, 'Answer added successfully')
 
-    def test_expose_add_question_failure(self):
+    def test_expose_failure_03(self):
         """
-        The test case is failing at line 81 
-        in add_question of quizzes_controller.py file 
+
+        The code encounters a crash at line 81 at add_question in quiz_controller.py file when attempting to add a question.
+        The issue originates from passing a date as text in the add_question function, and the program fails
+        to handle the exception appropriately.
+ 
         """
         self.ctrl.clear_data()
+        # Add a quiz
         quiz_id = self.ctrl.add_quiz("Quiz Title", "quiz 1", datetime.datetime(2023, 1, 3), datetime.datetime(2023, 1, 4))
-        question_id = self.ctrl.add_question(quiz_id, datetime.datetime.now(), 44)
+        # Adding a question
+        question_id = self.ctrl.add_question(quiz_id, datetime.datetime.now(), "Text of the question")
         self.assertIsNotNone(question_id, 'Question added successfully')
    
 
